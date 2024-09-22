23f8c46bc5f26b2180a8a3fc43cc60d00c0387a5
Ronitanand0000 <113673966+Ronitanand0000@users.noreply.github.com>
2024-09-22 20:45:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Ghost
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7412698601:AAGPnsp16TWWqFAThg4s7zOEJe6OK2Ko3P8")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("RONIT_IN")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7246521618")
+
+
+
+
+
+  
